106cc72ff11f211c2b74bfb18ca08b7b77f7843a
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-02-28 02:16:58 +0000

    criando classe MadaDiretaCsv

---
+++ malaDireta-Cliente/src/main/java/maladireta/maladireta/MalaDiretaCsv.java
@@ -0,0 +1,19 @@
+package maladireta.maladireta;
+
+import com.algaworks.maladireta.MalaDireta;
+import com.algaworks.maladireta.contato.Contatos;
+import maladireta.csv.ContatosCSV;
+
+public class MalaDiretaCsv extends MalaDireta {
+
+    private String nomeDoArquivo;
+
+    public MalaDiretaCsv(String nomeDoArquivo) {
+        this.nomeDoArquivo = nomeDoArquivo;
+    }
+
+    @Override
+    protected Contatos criarContatos() {
+        return new ContatosCSV(nomeDoArquivo);
+    }
+}
